4ad73ad7c98e7a1a1cd4711abd5dc63acc5551c4
Dima <laskovdmit@gmail.com>
2021-10-13 18:20:52 +0500

    Добавлена функциональность слайдеров

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import modals from './modules/modals';
+import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
     
     modals();
+    sliders('.feedback-slider-item', 'horisontal', '.main-prev-btn', '.main-next-btn' );
+    sliders('.main-slider-item', 'vertical');
 });
